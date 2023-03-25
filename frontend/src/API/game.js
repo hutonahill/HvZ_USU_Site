83574dc36098534ebeec764e54d9859d3e22e1ea
expely <josephhstay@gmail.com>
2023-03-25 13:23:52 -0600

    fixed api

--- frontend/src/API/game.js
+++ frontend/src/API/game.js
@@ -3,5 +3,9 @@ import axios from "axios";
 export function getGameState() {
     let result = '';
     axios.get('localhost:5000/game-state').then((res) => JSON.parse(res)).then((res) => result = res).catch((err) => console.log(err));
+    fetch("http://example.com/movies.json")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
     return result;
 }
--- frontend/src/API/getUser.js
+++ frontend/src/API/getUser.js
@@ -4,5 +4,11 @@ import { useState } from "react";
 export function getUser(uid) {
     let result = [];
     axios.get(`http://localhost:5000/getSingleUserData/${uid}`).then((res) => JSON.parse(res)).then((res) => result = res).catch((err) => console.log(err))
+    fetch(`http://localhost:5000/getSingleUserData/${uid}`)
+  .then((response) => response.json())
+  .then((data) => 
+        {console.log(data)
+        result = data.userData[1]});
+
     return result;
 }
