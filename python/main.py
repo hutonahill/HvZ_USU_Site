88de50d04770b5a60f08d78d8ad95438b72e4266
hutonahill <35947141+hutonahill@users.noreply.github.com>
2023-03-25 11:38:22 -0600

    rebuilt SQL db, added more functions

--- python/main.py
+++ python/main.py
@@ -126,7 +126,7 @@ def SQL_example():
     #print(type(connection))
 
     # Define the SELECT query and parameters (if any)
-    query = "SELECT * FROM players WHERE player_id = %s"
+    query = "SELECT * FROM players WHERE player_id = %s;"
     params = (1,)
 
     # Execute the SELECT query and get the results
@@ -214,12 +214,12 @@ def registerUserInGame(user_id:str, tag_code:str, gameName:str, stateName:str = 
     '''
 
     # start by geting the player_id from the user_id
-    query = (f"SELECT player_id FROM players WHERE user_id = %s")
+    query = (f"SELECT player_id FROM players WHERE user_id = %s;")
 
     perams = (user_id,)
     
 
-    results = SQL_SELECT (query, params)
+    results = SQL_SELECT (query, perams)
     
     
     # Get the filename with extension
@@ -229,14 +229,14 @@ def registerUserInGame(user_id:str, tag_code:str, gameName:str, stateName:str = 
     filename_without_extension = os.path.splitext(filename)[0]
 
     # run a battery of tests on the result to make sure its in the propper format
-    isValid = checkSingleSelectOutput(results)
+    isValid = checkSingleOutputSelect(results)
 
     # if checkSingeSelectOutput, return its errorMsg
     if (isValid != True):
         return isValid
     
     # make sure that row is holding an int
-    elif (is_int(result[1][0])):
+    elif (is_int(results[1][0])):
         
         return (f"ERROR: {filename_without_extension}.{registerUser.__name__}() " + 
             f"expected {filename_without_extension}.SQL_SELECT() to return a player_id " + 
@@ -244,19 +244,19 @@ def registerUserInGame(user_id:str, tag_code:str, gameName:str, stateName:str = 
 
     # if no errors were called, set player_id to the result
     else:
-        player_id = int(result[1][0])
+        player_id = int(results[1][0])
 
 
 
     # now get the game_id from the gameName
-    query = (f"SELECT game_id FROM games WHERE game_name = %s")
+    query = (f"SELECT game_id FROM games WHERE game_name = %s;")
 
     perams = (gameName,)
 
-    results = SQL_SELECT (query, params)
+    results = SQL_SELECT (query, perams)
 
     # run a battery of tests on the result to make sure its in the propper format
-    isValid = checkSingleSelectOutput(results)
+    isValid = checkSingleOutputSelect(results)
     
     # if it returns true there were no errors, if not it returned an errorMsg,
     # return it to the user.
@@ -264,28 +264,28 @@ def registerUserInGame(user_id:str, tag_code:str, gameName:str, stateName:str = 
         return isValid
     
     # make sure that row is holding an int
-    elif (is_int(result[1][0])):
+    elif (is_int(results[1][0])):
         return (f"ERROR: {filename_without_extension}.{registerUser.__name__}() " + 
             f"expected {filename_without_extension}.SQL_SELECT() to return a player_id " + 
             f"(int) but it did not")
 
     # if there were no errors, set the game_id as the result
     else:
-        game_id = int(result[1][0])
+        game_id = int(results[1][0])
 
 
-    
 
-    # ok, last item we need to look up, the code_id
 
-    query = (f"SELECT tag_code_id FROM codes WHERE tag_code = %s")
+    # look up, the code_id based on code
+
+    query = (f"SELECT tag_code_id FROM codes WHERE tag_code = %s;")
 
     perams = (gameName,)
 
-    results = SQL_SELECT(query, params)
+    results = SQL_SELECT(query, perams)
 
     # run a battery of tests on the result to make sure its in the propper format
-    isValid = checkSingleSelectOutput(results)
+    isValid = checkSingleOutputSelect(results)
     
     # if it returns true there were no errors, if not it returned an errorMsg,
     # return it to the user.
@@ -293,38 +293,294 @@ def registerUserInGame(user_id:str, tag_code:str, gameName:str, stateName:str = 
         return isValid
     
     # make sure that row is holding an int
-    elif (is_int(result[1][0])):
+    elif (is_int(results[1][0])):
         return (f"ERROR: {filename_without_extension}.{registerUser.__name__}() " + 
             f"expected {filename_without_extension}.SQL_SELECT() to return a player_id " + 
             f"(int) but it did not")
 
     # if there were no errors, set the game_id as the result
     else:
-        code_id = int(result[1][0])
+        code_id = int(results[1][0])
+    
 
+
+    # ok, last item we need to look up, the state_id, usualy based off `zombie``.
+    query = (f"SELECT state_id FROM states WHERE state = %s;")
+
+    perams = (stateName,)
+
+    results = SQL_SELECT(query, perams)
+
+    # run a battery of tests on the result to make sure its in the propper format
+    isValid = checkSingleOutputSelect(results)
+    
+    # if it returns true there were no errors, if not it returned an errorMsg,
+    # return it to the user.
+    if (isValid != True):
+        return isValid
+    
+    # make sure that row is holding an int
+    elif (is_int(results[1][0])):
+        return (f"ERROR: {filename_without_extension}.{registerUser.__name__}() " + 
+            f"expected {filename_without_extension}.SQL_SELECT() to return a player_id " + 
+            f"(int) but it did not")
+
+    # if there were no errors, set the game_id as the result
+    else:
+        state_id = int(results[1][0])
 
     # run the insert statment
     query = ("INSERT INTO players_games " +
     "(player_id, game_id, tag_code_id, states_id) " +
     "VALUES " +
-    "(%s, %s, %s);")
+    "(%s, %s, %s, %s);")
 
-    params = (player_id, game_id, code_id)    
+    params = (player_id, game_id, code_id, state_id)    
     
-    result = SQL_INSERT(connection, query, params)
+    results = SQL_INSERT(query, params)
 
     #if there was an error, reutrn the text of the error
-    if (type(result) == str):
-        return result
+    if (type(results) == str):
+        return results
 
-    # if there was no error, 
+    # if there was no error, return the json.
     else:
-        if (result != 1):
+        # make sure the json contians 1. otherwise theres a problem.
+        if (results != 1):
             return (f"ERROR: the qurry was only suposed to effect 1 row, "+
-            f"but it effected {result}")
+            f"but it effected {results}")
         
         else:
-            return json.dump({"rowsEffected":result})
+            return json.dump({"rowsEffected":results})
+
+@api.route('/getUserData/<user_id>')
+def getUserData(user_id:str):
+    '''
+    returns all user data assoceated with a user_id
+
+    returns a string if there was an error
+    returns a json in the below format if everything went smothely 
+
+    {"userData":nestedList}
+    '''
+    
+
+    query = (f"SELECT * FROM players WHERE user_id = %s;")
+
+    perams = (user_id,)
+    
+    results = SQL_SELECT (query, perams)
+    
+
+    # Get the filename with extension
+    filename = os.path.basename(__file__)
+
+    # Extract the filename without extension (used in returning errors)
+    filename_without_extension = os.path.splitext(filename)[0]
+
+    # if results is not a list there was an error
+    if (type(results != list)):
+        # if its a string, it contains an error msg
+        if (type(results) == str):
+            return results
+        # if not, gerate our own.
+        else:
+            return (f"ERROR: {filename_without_extension}.{registerUser.__name__}() " + 
+                f"expected {filename_without_extension}.SQL_SELECT() to return a list " + 
+                f"but it returned a `{type(results)}`")
+        
+    # make sure there are only two rows, the data we want and a header.
+    elif (len(results) != 2):
+        return (f"ERROR: {filename_without_extension}.{registerUser.__name__}() " + 
+            f"expected {filename_without_extension}.SQL_SELECT() to return two rows " + 
+            f"but it returned {len(results)}")
+    
+    # make sure every row is a list
+    for i in range(len(results)):
+        row = results[i]
+
+        if (type(row) != list):
+            return (f"ERROR: {filename_without_extension}.{registerUser.__name__}() " + 
+                f"expected {filename_without_extension}.SQL_SELECT() to " + 
+                f"return a nested list, but row  {i} is a" + 
+                f"'{type(row)}'")
+    
+    return json.dumps({"userData":results})
+
+@api.route('/getUserData')
+def getUserData():
+    '''
+    returns all user data.
+
+    returns a string if there was an error
+    returns a json in the below format if everything went smothely 
+
+    {"userData":nestedList}
+    '''
+    
+    query = (f"SELECT * FROM players;")
+
+    
+    results = SQL_SELECT (query)
+    
+
+    # Get the filename with extension
+    filename = os.path.basename(__file__)
+
+    # Extract the filename without extension (used in returning errors)
+    filename_without_extension = os.path.splitext(filename)[0]
+
+    # if results is not a list there was an error
+    if (type(results != list)):
+        # if its a string, it contains an error msg
+        if (type(results) == str):
+            return results
+        # if not, gerate our own.
+        else:
+            return (f"ERROR: {filename_without_extension}.{registerUser.__name__}() " + 
+                f"expected {filename_without_extension}.SQL_SELECT() to return a list " + 
+                f"but it returned a `{type(results)}`")
+    
+    # make sure every row is a list
+    for i in range(len(results)):
+        row = results[i]
+
+        if (type(row) != list):
+            return (f"ERROR: {filename_without_extension}.{registerUser.__name__}() " + 
+                f"expected {filename_without_extension}.SQL_SELECT() to " + 
+                f"return a nested list, but row  {i} is a" + 
+                f"'{type(row)}'")
+    
+    return json.dumps({"userData":results})
+
+@api.route('/getTags')
+def getTags():
+    '''
+    returns all tags.
+
+    returns a string if there was an error
+    returns a json in the below format if everything went smothely 
+
+    {"tags":nestedList}
+    '''
+    # first we get the active game
+    # now get the game_id from the gameName
+    query = (f"SELECT game_id FROM games WHERE is_game_active = y;")
+
+    results = SQL_SELECT (query)
+
+    # run a battery of tests on the result to make sure its in the propper format
+    isValid = checkSingleOutputSelect(results)
+    
+    # if it returns true there were no errors, if not it returned an errorMsg,
+    # return it to the user.
+    if (isValid != True):
+        return isValid
+    
+    # make sure that row is holding an int
+    elif (is_int(results[1][0])):
+        return (f"ERROR: {filename_without_extension}.{registerUser.__name__}() " + 
+            f"expected {filename_without_extension}.SQL_SELECT() to return a player_id " + 
+            f"(int) but it did not")
+
+    # if there were no errors, set the game_id as the result
+    else:
+        game_id = int(results[1][0])
+    
+
+    query = (f"SELECT * FROM tag_registry WHERE game_id = %s;")
+
+    perams = (game_id, )
+    
+    results = SQL_SELECT (query)
+    
+
+    # Get the filename with extension
+    filename = os.path.basename(__file__)
+
+    # Extract the filename without extension (used in returning errors)
+    filename_without_extension = os.path.splitext(filename)[0]
+
+    # if results is not a list there was an error
+    if (type(results != list)):
+        # if its a string, it contains an error msg
+        if (type(results) == str):
+            return results
+        # if not, gerate our own.
+        else:
+            return (f"ERROR: {filename_without_extension}.{registerUser.__name__}() " + 
+                f"expected {filename_without_extension}.SQL_SELECT() to return a list " + 
+                f"but it returned a `{type(results)}`")
+    
+    # make sure every row is a list
+    for i in range(len(results)):
+        row = results[i]
+
+        if (type(row) != list):
+            return (f"ERROR: {filename_without_extension}.{registerUser.__name__}() " + 
+                f"expected {filename_without_extension}.SQL_SELECT() to " + 
+                f"return a nested list, but row  {i} is a" + 
+                f"'{type(row)}'")
+    
+    return json.dumps({"tags":results})
+
+@api.route('/new2FAKey') 
+def new2FAKey():
+    from pyotp import random_base32
+
+    return json.dump(random_base32())
+
+@api.route('/check2FAKey/<user_id>/<code>') 
+def check2FAKey(user_id, code):
+    
+    query = (f"SELECT twoFA_key FROM players WHERE user_id = %s;")
+
+    perams = (user_id,)
+    
+    results = SQL_SELECT (query, perams)
+    
+
+    # Get the filename with extension
+    filename = os.path.basename(__file__)
+
+    # Extract the filename without extension (used in returning errors)
+    filename_without_extension = os.path.splitext(filename)[0]
+
+    # if results is not a list there was an error
+    if (type(results != list)):
+        # if its a string, it contains an error msg
+        if (type(results) == str):
+            return results
+        # if not, gerate our own.
+        else:
+            return (f"ERROR: {filename_without_extension}.{registerUser.__name__}() " + 
+                f"expected {filename_without_extension}.SQL_SELECT() to return a list " + 
+                f"but it returned a `{type(results)}`")
+        
+    # make sure there are only two rows, the data we want and a header.
+    elif (len(results) != 2):
+        return (f"ERROR: {filename_without_extension}.{registerUser.__name__}() " + 
+            f"expected {filename_without_extension}.SQL_SELECT() to return two rows " + 
+            f"but it returned {len(results)}")
+    
+    # make sure every row is a list
+    for i in range(len(results)):
+        row = results[i]
+
+        if (type(row) != list):
+            return (f"ERROR: {filename_without_extension}.{registerUser.__name__}() " + 
+                f"expected {filename_without_extension}.SQL_SELECT() to " + 
+                f"return a nested list, but row  {i} is a" + 
+                f"'{type(row)}'")
+    
+    secretKey = results[0][0]
+    
+    from pyotp import TOTP
+
+    totp = TOTP(secretKey)
+    authenticated = totp.verify(code)
+
+    return authenticated
 
 def is_int(s):
     try:
@@ -333,7 +589,7 @@ def is_int(s):
     except ValueError:
         return False
 
-def checkSingleSelectOutput(results):
+def checkSingleOutputSelect(results):
     '''
     checks if a SELECT query designed to return a single result did so.
 
@@ -384,5 +640,7 @@ def checkSingleSelectOutput(results):
 if __name__ == "__main__":
     SQL_example()
 
+# THIS LINE MUST BE UNCOMMENTED IN ORDER FOR THE PROGRAM TO WORK 
+# AS AN API!
 # if __name__ == '__main__':
 #     api.run()
